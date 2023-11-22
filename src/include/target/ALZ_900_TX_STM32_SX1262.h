74b1960435e702cabbb2d7715e85662dda8dd0c9
liuyinjia <alezh.liu@gmail.com>
2023-11-22 10:35:14 +0800

    stm32

--- src/include/target/ALZ_900_TX_STM32_SX1262.h
+++ src/include/target/ALZ_900_TX_STM32_SX1262.h
@@ -2,39 +2,60 @@
 #define DEVICE_NAME "ELRS 900TX"
 #endif
 
-#define TARGET_DIY_900_TX_STM32_PCB
+#define TARGET_ALZ_900_TX_STM32_PCB
 
 #define BACKPACK_LOGGING_BAUD 420000
 
+#if !defined(USE_OLED_SPI_SMALL)
+    #define USE_OLED_SPI
+#endif
+
 // GPIO pin definitions
-#define GPIO_PIN_NSS         PA4
-#define GPIO_PIN_MOSI        PA7
-#define GPIO_PIN_MISO        PA6
-#define GPIO_PIN_SCK         PA5
+#define GPIO_PIN_NSS                PA15
+#define GPIO_PIN_BUSY               PB15
+#define GPIO_PIN_DIO1               PB2
+#define GPIO_PIN_MOSI               PA7
+#define GPIO_PIN_MISO               PA6
+#define GPIO_PIN_SCK                PA5
+#define GPIO_PIN_RST                PB0
 
-#define GPIO_PIN_RST         PB0
-#define GPIO_PIN_DIO0        PB1
-#define GPIO_PIN_DIO1        PB2
-#define GPIO_PIN_BUSY        PB3
+#define GPIO_PIN_TX_ENABLE          PA8  // Works on Lite
+#define GPIO_PIN_RX_ENABLE          PB14 // Works on Lite
 
-#define GPIO_PIN_RX_ENABLE   PA2
-#define GPIO_PIN_TX_ENABLE   PA3
+#define GPIO_PIN_ANT_CTRL           PA9
+#define GPIO_PIN_ANT_CTRL_COMPL     PB13
 
-#define GPIO_PIN_RCSIGNAL_RX PB11  // UART 3
-#define GPIO_PIN_RCSIGNAL_TX PB10  // UART 3
+#define GPIO_PIN_RCSIGNAL_RX        PA10 // S.PORT (Only needs one wire )  UART 3
+#define GPIO_PIN_RCSIGNAL_TX        PB6  // Needed for CRSF libs but does nothing/not hooked up to JR module.  UART 3
 
-#define GPIO_PIN_DEBUG_RX    PB7  // UART 1
-#define GPIO_PIN_DEBUG_TX    PB6  // UART 1
+/* WS2812 led */
+#define GPIO_PIN_LED_WS2812         PB6
+#define GPIO_PIN_LED_WS2812_FAST    PB_6
 
-#define GPIO_PIN_LED_GREEN   PA10
-#define GPIO_LED_GREEN_INVERTED 0
+#define GPIO_PIN_PA_ENABLE          PB11  // https://www.skyworksinc.com/-/media/SkyWorks/Documents/Products/2101-2200/SE2622L_202733C.pdf
+#define GPIO_PIN_RF_AMP_DET         PA3  // Voltage detector pin
 
-#define GPIO_PIN_LED GPIO_PIN_LED_GREEN
+#define GPIO_PIN_BUZZER             PC13
+#define GPIO_PIN_OLED_CS            PC14
+#define GPIO_PIN_OLED_RST           PB12
+#define GPIO_PIN_OLED_DC            PC15
+#define GPIO_PIN_OLED_MOSI          PB5
+#define GPIO_PIN_OLED_SCK           PB3
+#define GPIO_PIN_JOYSTICK           A0
 
-#define RADIO_SX1272
+#define GPIO_PIN_DEBUG_RX           PA3 // WTF! It's used for AMP_DET  UART 1
+#define GPIO_PIN_DEBUG_TX           PA2 // UART 1
+
+
+// #define RADIO_SX126X
 
 // Output Power
-#define MinPower                PWR_10mW
+#define MinPower                PWR_50mW
 #define MaxPower                PWR_1000mW
 #define DefaultPower            PWR_50mW
-#define POWER_OUTPUT_VALUES     {0,15,22}
+#define POWER_OUTPUT_VALUES     {0,10,14,17,22}
+
+#ifndef JOY_ADC_VALUES
+/* Joystick values              {UP, DOWN, LEFT, RIGHT, ENTER, IDLE}*/
+#define JOY_ADC_VALUES          {459, 509, 326, 182, 91, 1021}
+#endif
